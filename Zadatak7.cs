031a7e98a73e4af594a161f220105c4027979837
Luka Trempetić <trempetic92@gmail.com>
2024-02-05 17:22:26 +0100

    zad7 com

--- Zadatak7.cs
+++ Zadatak7.cs
@@ -96,6 +96,9 @@ namespace zadaci_za_vjezbu_M01_Z01
                     novaRecenica += znak;
                 }
             }
+
+            //Moglo se i sa 1 for petljom ako bi uzeli u bzor da novaRecenica != novaRecenica[i]
+
             char[] slovkamo = new char[novaRecenica.Length];
             Console.WriteLine($"Originalna rečenica: {recenica}");
             for (int i = 0; i < novaRecenica.Length; i++)
